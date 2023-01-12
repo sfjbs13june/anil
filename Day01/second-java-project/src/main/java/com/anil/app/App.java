201681ede41a4f95dd748a0dc23190b2aafcc72a
anil <anil.rayavarapu@tcs.com>
2023-01-12 06:45:03 +0000

    proectDay01

---
+++ Day01/second-java-project/src/main/java/com/anil/app/App.java
@@ -0,0 +1,13 @@
+package com.anil.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
