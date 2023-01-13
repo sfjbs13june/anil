5b85db77fb6adde00223d685b070d9377967f71f
anil <anil.rayavarapu@tcs.com>
2023-01-13 05:23:55 +0000

    First-springboot-app:Day02

---
+++ Day02/first-spring-boot-app/src/test/java/com/anil/app/FirstSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anil.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
